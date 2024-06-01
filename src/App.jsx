e2809857e5f564f6602b86988cc366fed2f86435
hayyantahirr <hayyantahir7@gmail.com>
2024-06-01 20:02:59 +0500

    all components rendered as per instructed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Card from "./components/card";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div className="flex flex-wrap gap-8 justify-center">
+        <Card
+          title="Nike Air"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 78000"
+        />
+        <Card
+          title="Nike 780"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 80000"
+        />
+        <Card
+          title="Nike beast"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 780000"
+        />
+        <Card
+          title="Nike force"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 95000"
+        />
+        <Card
+          title="Nike terminator"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 25000"
+        />
+        <Card
+          title="Nike safari"
+          des="These are the all new nike shoes designed by loius vuiton and gucci .These are...."
+          price="PKR 5 0000"
+        />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Card(prop) {
+  return (
+    <>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{prop.title}</h2>
+          <p>{prop.des}</p>
+          <p>{prop.price}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
